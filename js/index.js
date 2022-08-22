71994a62473e9b29c4dab05f2f12a3cf0c0238f2
Jonathan <jonathanmoorevla@gmail.com>
2022-08-21 20:42:11 -0500

    Add show details functionality

--- js/index.js
+++ js/index.js
@@ -6,7 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
         data.forEach(book => {
             let li = document.createElement('li')
             li.innerText = book.title
+            li.addEventListener('click', () => {
+                listDeets(book)
+            })
             titleContainer.appendChild(li)
         })
     })
 });
+
+function listDeets(bookObj) {
+    console.log(bookObj)
+    let preview = document.getElementById('show-panel')
+    preview.innerHTML = `
+    <img src='${bookObj.img_url}' alt='${bookObj.title} thumbnail'>
+    <h2>${bookObj.title}</h2>
+    <h2>${bookObj.subtitle}</h2>
+    <h2>${bookObj.author}</h2>
+    <p>${bookObj.description}</p>
+    `
+}
