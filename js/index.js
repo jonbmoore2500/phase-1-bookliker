718e92f794bb3d60bbf9a77e7df64a2820beaf93
Jonathan <jonathanmoorevla@gmail.com>
2022-08-21 20:33:09 -0500

    Add list books functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch(`http://localhost:3000/books`)
+    .then(resp => resp.json())
+    .then(data => {
+        let titleContainer = document.getElementById('list')
+        data.forEach(book => {
+            let li = document.createElement('li')
+            li.innerText = book.title
+            titleContainer.appendChild(li)
+        })
+    })
+});
